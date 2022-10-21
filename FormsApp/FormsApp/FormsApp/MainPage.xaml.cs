415361bf46bf4e6e561d22c53db3a735777fcbf2
iberis18 <air.1804@yabdex.ru>
2022-10-21 20:27:48 +0300

    change project

---
+++ FormsApp/FormsApp/FormsApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using FormsApp.view_model;
+
+namespace FormsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new PhoneViewModel
+            {
+                Title = "iPhone 7",
+                Company = "Apple",
+                Price = 52000
+            };
+        }
+    }
+}
